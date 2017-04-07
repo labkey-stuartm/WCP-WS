df112d761b0e1a2089dfe4f16649da92df7ee7d9
Mohan T <mohant@boston-technology.com>
2017-04-07 19:27:58 +0530

    Response Format updated for activityList WS

--- src/main/java/com/studymetadata/bean/ActivityResponse.java
+++ src/main/java/com/studymetadata/bean/ActivityResponse.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 public class ActivityResponse {
 	private String message = "FAILURE";
-	private List<ActivitiesBean> activities = new ArrayList<ActivitiesBean>(); //<refer Appendix 7 for structure>
-	private AnchorDateBean anchorDate = new AnchorDateBean();
-	private String withdrawalConfig = ""; //delete-data/ask-user/no-action
+	private List<ActivitiesBean> activities = new ArrayList<>();
 	
 	public String getMessage() {
 		return message;
@@ -21,17 +19,4 @@ public class ActivityResponse {
 	public void setActivities(List<ActivitiesBean> activities) {
 		this.activities = activities;
 	}
-	public AnchorDateBean getAnchorDate() {
-		return anchorDate;
-	}
-	public void setAnchorDate(AnchorDateBean anchorDate) {
-		this.anchorDate = anchorDate;
-	}
-	public String getWithdrawalConfig() {
-		return withdrawalConfig;
-	}
-	public void setWithdrawalConfig(String withdrawalConfig) {
-		this.withdrawalConfig = withdrawalConfig;
-	}
-	
 }
