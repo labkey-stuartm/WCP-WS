903a317b2ba25358cc5370d1b4195dd166a43438
MOHANT-PC\MoHanT <mohant@boston-technology.com>
2017-12-18 15:55:18 +0530

    1. Default values of kickCount and maxDuration are added

--- src/main/java/com/studymetadata/bean/FetalKickCounterFormatBean.java
+++ src/main/java/com/studymetadata/bean/FetalKickCounterFormatBean.java
@@ -3,6 +3,8 @@
  */
 package com.studymetadata.bean;
 
+import com.studymetadata.util.StudyMetaDataConstants;
+
 /**
  * @author Mohan
  * @createdOn Nov 3, 2017 12:22:36 PM
@@ -10,8 +12,8 @@ package com.studymetadata.bean;
  */
 public class FetalKickCounterFormatBean {
 	
-	private Integer duration = 0;
-	private Integer kickCount = 0;
+	private Integer duration = Integer.parseInt(StudyMetaDataConstants.FETAL_MAX_DURATION);
+	private Integer kickCount = StudyMetaDataConstants.MAX_KICK_COUNT;
 	
 	public Integer getDuration() {
 		return duration;
